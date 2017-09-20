a26acdb5df25807ce7fd861cc7aa4bab6ef16a44
viniciusrcamargo <viniciusr.camargo@gmail.com>
2017-09-20 01:00:36 -0300

    Test in Java

---
+++ ProjetoAulaUdemy/src/com/projeto/variaveisjava/Variavel.java
@@ -0,0 +1,25 @@
+package com.projeto.variaveisjava;
+
+public class Variavel {
+
+	public static void main(String[] args) {
+		
+		byte varByte = 13;
+		int varInt = 12500;
+		long varLong = 100000000;
+		float varFlot = 100.50f;
+		double varDouble = 500.40;
+		boolean varBoolean = true;
+		String varString = "texto String";
+		
+		System.out.println("Variável byte = " + varByte);
+		System.out.println("Variável int = " + varInt);
+		System.out.println("Variável long = " + varLong);
+		System.out.println("Variável float = " + varFlot);
+		System.out.println("Variável double = " + varDouble);
+		System.out.println("Variável boolean = " + varBoolean);
+		System.out.println("Variável String = " + varString);
+
+	}
+
+}
