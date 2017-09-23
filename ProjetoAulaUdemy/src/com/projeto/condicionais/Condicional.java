bfbd6a45de3b60c8727e8c063176c3d2f44d9499
viniciusrcamargo <viniciusr.camargo@gmail.com>
2017-09-23 11:49:59 -0300

    Tests in Java

---
+++ ProjetoAulaUdemy/src/com/projeto/condicionais/Condicional.java
@@ -0,0 +1,18 @@
+package com.projeto.condicionais;
+
+public class Condicional {
+
+	public static void main(String[] args) {
+		
+		boolean test = false;
+		int media = 7;
+		
+		if(media < 6) {
+			System.out.println("reprovado");
+		}else if(media >= 7){
+			System.out.println("aprovado");
+		}
+
+	}
+
+}
